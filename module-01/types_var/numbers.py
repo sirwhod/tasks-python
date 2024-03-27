497ba8e8f195e5201c3bfe5305c51371b1453de7
Rodrigo <rodrigo.brandao98@gmail.com>
2024-03-27 10:32:33 -0300

    class: Números - Parte 2

--- module-01/types_var/numbers.py
+++ module-01/types_var/numbers.py
@@ -11,3 +11,39 @@ print("Real com ponto flutuante = ", real_number)
 # Type() | Objetivo é retornar o tipo da variavel
 print("Tipo da variavel inteiro = ", type(int_number))
 print("Tipo da variavel real = ", type(real_number))
+
+# Operações aritiméticas
+
+# Soma +
+sum = 1 + 1
+print("1 + 1 = ", sum)
+
+# Subtração -
+sub = 1 - 1
+print("1 - 1 = ", sub)
+
+# Multiplicação *
+multi = 2 * 2
+print("2 x 2 = ", multi)
+
+# Divisão /
+div = 5 / 2
+print("5 / 2 = ", div)
+print("Tipo do resultado da divisão = ", type(div))
+
+# Converter float pra int()
+print("Valor em inteiro = ", int(div))
+print("Tipo do resultado da divisão depois de transformar em inteiro = ", type(int(div)))
+
+# Converter int pra float()
+print("Valor em Real = ", float(sum))
+print("Tipo do resultado da soma depois de transformar em real = ", type(float(div)))
+
+# Módulo % (Resto da divisão)
+module = 5 % 2
+print("Módulo = ", module)
+
+# Divisão Inteiro //
+int_div = 5 // 2
+print("5 // 2 = ", int_div)
+print("Tipo do resultado da divisão inteiro = ", type(int_div))

004d83a793a680bf455b0175b5fea2fca9391c5f
Rodrigo <rodrigo.brandao98@gmail.com>
2024-03-27 10:55:44 -0300

    class: Texto - Parte 1

---
+++ module-01/types_var/text.py
@@ -0,0 +1,17 @@
+# Declaração (Por padrão utilizar aspas duplas "")
+complete_name = "Rodrigo Brandão"
+# utilizando 6x Aspas duplas """ """ poderá incluir novas linhas dentro da string
+complete_name_jumping = """Rodrigo
+Brandão
+25
+anos
+"""
+
+# Utilizando a \ a quebra de linha também é realizada
+complete_name_broke = "Rodrigo \
+Brandão \
+25 \
+anos"
+
+name = "Rodrigo"
+surname = "Brandão"

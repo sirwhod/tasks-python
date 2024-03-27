c15086246d3b8152f67c977236eca770f499b94b
Rodrigo <rodrigo.brandao98@gmail.com>
2024-03-27 10:09:46 -0300

    class: Números - Parte 1

---
+++ module-01/types_var/numbers.py
@@ -0,0 +1,13 @@
+# O python oferece diversos tipos de números, são eles:
+
+# Inteiros
+int_number = 4
+print("Inteiro = ", int_number)
+
+# Real com ponto flutuante
+real_number = 3.14
+print("Real com ponto flutuante = ", real_number)
+
+# Type() | Objetivo é retornar o tipo da variavel
+print("Tipo da variavel inteiro = ", type(int_number))
+print("Tipo da variavel real = ", type(real_number))

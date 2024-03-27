d9e7ea47e2dc0eadc5e367e8e4d03f3d3eb1347e
Rodrigo <rodrigo.brandao98@gmail.com>
2024-03-27 10:02:26 -0300

    class: introdução a variáveis

---
+++ module-01/var.py
@@ -0,0 +1,11 @@
+# Variáveis devem começar com letras sem acento ou underline
+# São case sensitives
+
+# Strings (snake_case) | variáveis, funções e métodos
+complete_name = 'Rodrigo Brandão'
+
+# Strings (camelCase) | para Classes
+completeName = 'Rodrigo Brandão'
+
+# Documentação do: Style Guide for Python Code
+# https://peps.python.org/pep-0008/
